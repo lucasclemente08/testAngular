2ce6f1b52788bffe5fb6bc5d5d155a7baf0d318a
lucasclemente08 <lucasclemente2016@gmail.com>
2025-05-25 19:49:04 -0300

    First commit

---
+++ src/app/app.routes.ts
@@ -0,0 +1,21 @@
+import { Routes } from '@angular/router';
+import { SearchComponent } from './search/search.component';
+import { BookingComponent } from '../pages/booking/booking.component';
+
+export const routes: Routes = [
+
+{
+    path: '',
+    redirectTo: 'search',
+    pathMatch: 'full'
+},
+{
+    path: 'search',
+    component:SearchComponent
+},
+{
+    path: 'booking',
+    component: BookingComponent
+}
+
+];
