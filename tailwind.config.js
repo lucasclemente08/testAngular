7d23c1ab3a6111fe9a70d417b7f99c26c5e465dc
lucasclemente08 <lucasclemente2016@gmail.com>
2025-06-04 13:05:59 -0300

    Add firts components

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  darkMode: 'false', // Opciones: 'media', 'class' o false
+  plugins: [],
+}
